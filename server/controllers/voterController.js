c0e5f9f4624977bb6607be31c3890af4957f5672
saloni9142 <kumarisaloni762@gmail.com>
2025-10-16 18:02:53 +0530

    voters added

--- server/controllers/voterController.js
+++ server/controllers/voterController.js
@@ -1,23 +1,107 @@
+const bcrypt =require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const VoterModel= require('../models/voterModel')
+const HttpError= require('../models/ErrorModel')
+
 // register new voter
+
 // POST: api/voters/register
 // unprotected
 const registerVoter = async(req, res,next)=>{
-    res.json("Register Voter")
+    try{
+        const {fullName, email, password, password2}= req.body;
+        if(!fullName || !email || !password || !password2){
+            return next(new HttpError('fill in all fields', 422))
+        }
+        // make all emails lowercased
+        const newEmail = email.toLowerCase()
+        // /check if the email alraedy exits in db
+        const emailExits = await VoterModel.findOne({email: newEmail})
+if(emailExits){
+            return next(new HttpError ("Email already exist.", 422))
+        }
+//  make sure password 6+ characters
+if((password.trim().length) <6){
+    return next(new HttpError("password should be at least 6 charcters.",422))
 }
 
+// make sure paswords match
+if(password  !== password2){
+      return next(new HttpError("password do not match.",422))
+}
+// hash password
+const salt= await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(password, salt);
+
+// no user/voter should be admin except for one with email "achiever@gmail.com"
+  let isAdmin = false;
+  if(newEmail=== "saloni@gmail.com"){
+    isAdmin = true
+  }
+//   save new voter to db
+const newVoter = await VoterModel.create({fullName, email: newEmail, password: hashedPassword, isAdmin})
+res.status(201).json(`New voter ${fullName} created.`)
+
+
+    } catch (error){
+        console.log("error details: error");
+        console.log("Error message:", error.message); 
+return next(new HttpError("voter registration failed.",422))
+}
+
+}
+
+
+// function to generate token
+const generateToken =(payload) =>{
+    const token = jwt.sign(payload, process.env.JWT_SECRET,{expiresIn:"2d"})
+    return token;
+}
 
 // login  voter
 // POST: api/voters/login
 // unprotected
 const loginVoter = async(req, res,next)=>{
-    res.json("login Voter")
+   try{
+    const{email, password} =req.body;
+    if(!email || !password){
+        return next(new HttpError("fill in all fields", 422))
+    }
+    const newEmail = email.toLowerCase()
+    const voter= await VoterModel.findOne({email: newEmail})
+    if(!voter){
+        return next(new HttpError("invalid credentials",422))
+    }
+    // compare passwords
+    const comparePass= await bcrypt.compare(password, voter.password)
+    if(!comparePass){
+        return next(new HttpError("Invalid credentials",422))
+    }
+    const {_id: id, isAdmin, votedElection}=voter;
+    const token= generateToken({id, isAdmin})
+    res.json({token, id, votedElection, isAdmin})
+
+
+   } catch(error){
+    return next(new HttpError("login failed. Please check your credentials or try again later.",422))
+   }
 }
+
+
+
 
 // get voter
 // GET: api/voters/:id 
 // protected
 const getVoter = async(req, res,next)=>{
-    res.json("get Voter")
+    try{
+        const {id} =req.params;
+        const voter = await VoterModel.findById(id).select("-password")
+        res.json(voter)
+
+    } catch(error){
+        return next (HttpError("could not get the voter"))
+    }
 }
 
 
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const {notFound, errorHandler} =require("./middleware/errorMiddleware")
 
 
  const app =express()
- app.use(express.json({extended: true}))
+ app.use(express.json())
  app.use(express.urlencoded({extended:true}))
  app.use(cors({credentials: true, origin:["http://localhost:3000"]}))
  app.use('/api',Routes)
